7f5746280f17a8a5e305bf1b6450e8b52617d259
Leakfa <83780711+Leakfarsi@users.noreply.github.com>
2022-01-09 23:23:07 +0330

    Create footer.php

---
+++ src/footer.php
@@ -0,0 +1,11 @@
+<br/>
+<br/>
+<br/>
+
+<footer class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <p>سامانه ردیابی نشت اطلاعات ایرانیان<br/>کپی رایت © 2022 همه حقوق محفوظ است.</p>
+    </div>
+</footer>
+</body>
+</html>
