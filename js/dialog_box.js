4b849f8347feb663c2748b62a1f4be6f752b3844
Leakfa <83780711+Leakfarsi@users.noreply.github.com>
2022-01-09 19:50:18 +0330

    Create dialog_box.js

---
+++ js/dialog_box.js
@@ -0,0 +1,14 @@
+function myFunction()
+{
+var x;
+var r=confirm;javascript:window.prompt ('Please send a donation to our Bitcoin address:', '19hNF1bmQmioBsvUq8fDYDqgJDHPJ7z1T2');
+if (r==true)
+  {
+  x="&#9829; !Thank you";
+  }
+else
+  {
+  x="&#9829; !Thank you";
+  }
+document.getElementById("msg").innerHTML=x;
+}
