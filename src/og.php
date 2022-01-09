c603be95c4ed35a455ebd59ae0bf58a9399d6d86
Leakfa <83780711+Leakfarsi@users.noreply.github.com>
2022-01-09 23:26:38 +0330

    Create og.php

---
+++ src/og.php
@@ -0,0 +1,14 @@
+<meta name="description" content="این سرویس یک سامانه ردیابی و جستجوی نشت اطلاعات شخصی است که برای ایرانیان طراحی شده است. به کاربران اطلاع می دهد که آیا آنها در لیست نقض های امنیتی بزرگ وجود دارند یا خیر و تلاش می کند تا سواد امنیتی مردم را افزایش می دهند." />
+<meta property="og:title" content="<?=isset($title) ? $title.' | ':''?>سامانه ردیابی نشت اطلاعات ایرانیان" />
+<meta property="og:type" content="website" />
+<meta property="og:description" content="This service is a personal information leakage tracking and inquiry system designed for Iran, which can effectively inquire a personal information leakage channel. Let users know whether they exist in the list of major security breaches, and increase their information security literacy." />
+<meta property="og:image" content="https://leakfa.com/images/logo.png" />
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXXX-X"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-XXXXXXXXX-X');
+</script>
