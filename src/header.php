c5ada4fe19770688c9e54be710a6f4128bdb4d94
Leakfa <83780711+Leakfarsi@users.noreply.github.com>
2022-01-13 15:40:03 +0330

    Update header.php

--- src/header.php
+++ src/header.php
@@ -24,7 +24,7 @@ require 'config.php';
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark">
             <div class="container">
-                <a class="navbar-brand" href="/">
+                <a class="navbar-brand" href="index.php">
                     <img src="/images/logo.svg" width="30" height="30" class="d-inline-block align-top" style="filter: invert(1);" alt="" />
                     لیکفا | Leakfa
                 </a>
@@ -35,28 +35,28 @@ require 'config.php';
                 <div class="collapse navbar-collapse" id="navbar">
                     <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
                         <?php $uri = $_SERVER['REQUEST_URI']; ?>
-                        <li class="nav-item <?= $uri == '/search' ? 'active' : '' ?>">
-                            <a class="nav-link" href="search">جستجوی نشت</a>
+                        <li class="nav-item <?= $uri == '/search.php' ? 'active' : '' ?>">
+                            <a class="nav-link" href="search.php">جستجوی نشت</a>
                         </li>
-                        <li class="nav-item <?= $uri == '/leaks' ? 'active' : '' ?>">
-                            <a class="nav-link" href="leaks">نشت‌های عمده</a>
+                        <li class="nav-item <?= $uri == '/leaks.php' ? 'active' : '' ?>">
+                            <a class="nav-link" href="leaks.php">نشت‌های عمده</a>
                         </li>
-                        <li class="nav-item <?= $uri == '/notify' ? 'active' : '' ?>">
-                            <a class="nav-link" href="notify">باخبرم کن</a>
+                        <li class="nav-item <?= $uri == '/notify.php' ? 'active' : '' ?>">
+                            <a class="nav-link" href="notify.php">باخبرم کن</a>
                         </li>
-                        <li class="nav-item <?= $uri == '/donate' ? 'active' : '' ?>">
-                            <a class="nav-link" href="donate">حمایت مالی</a>
+                        <li class="nav-item <?= $uri == '/donate.php' ? 'active' : '' ?>">
+                            <a class="nav-link" href="donate.php">حمایت مالی</a>
                         </li>
-                        <li class="nav-item <?= $uri == '/faq' ? 'active' : '' ?>">
-                            <a class="nav-link" href="faq">سوالات متداول</a>
+                        <li class="nav-item <?= $uri == '/faq.php' ? 'active' : '' ?>">
+                            <a class="nav-link" href="faq.php">سوالات متداول</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">سایر</a>
                             <div class="dropdown-menu">
-							    <a class="dropdown-item <?= $uri == '/policy' ? 'active' : '' ?>" href="policy">خط مشی</a>
-                                <a class="dropdown-item <?= $uri == '/about' ? 'active' : '' ?>" href="about">درباره ما</a>
+                                <a class="dropdown-item <?= $uri == '/policy.php' ? 'active' : '' ?>" href="policy.php">خط مشی</a>
+                                <a class="dropdown-item <?= $uri == '/about.php' ? 'active' : '' ?>" href="about.php">درباره ما</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item <?= $uri == '/stats' ? 'active' : '' ?>" href="stat">آمار ها</a>
+                                <a class="dropdown-item <?= $uri == '/stat.php' ? 'active' : '' ?>" href="stat.php">آمار ها</a>
                             </div>
                         </li>
                     </ul>
