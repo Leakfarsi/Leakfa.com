b7b5eae7cd44823a18c255246791fdba841cd95d
Leakfa <83780711+Leakfarsi@users.noreply.github.com>
2024-03-08 00:55:55 +0330

    Update footer.php

--- src/footer.php
+++ src/footer.php
@@ -4,7 +4,7 @@
 
 <footer class="jumbotron jumbotron-fluid">
     <div class="container">
-        <p>سامانه ردیابی نشت اطلاعات ایرانیان<br/>کپی رایت © 2023 همه حقوق محفوظ است.</p>
+        <p>سامانه ردیابی نشت اطلاعات ایرانیان<br/>کپی رایت © 2024 همه حقوق محفوظ است.</p>
     </div>
 </footer>
 </body>
