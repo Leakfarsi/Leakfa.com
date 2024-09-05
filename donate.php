3a4799a1bd316710ed6290a29e79019c484035c3
Leakfa <83780711+Leakfarsi@users.noreply.github.com>
2024-09-06 00:11:40 +0330

    Update donate.php

--- donate.php
+++ donate.php
@@ -8,15 +8,26 @@
     </div>
 </header>
 
-    <div class="padded container">
-        <h1 class="breach-title">حمایت مالی از سرویس</h1>
-        <p>لیکفا، یک سرویس رایگان و بدون تبلیغات برای مردم، اما مدیریت آن برای ما بدون داشتن حامی بسیار پرهزینه و دشوار است، شما میتوانید برای حمایت از هزینه‌های پروژه با ارسال بیت‌کوین به روند پیشرفت و رشد سرویس کمک کنید!
-            <p/>
-            <p>با کلیک بر روی دکمه "Donate Crypto" به درگاه Plisio انتقال یافته و امکان انتخاب ارز دلخواه خود برای انتقال آسان از هر کیف پولی در اختیار شما قرار خواهد گرفت.</p>
-		<center>
-    <a href="https://plisio.net/donate/maM39JmI" target="_blank">
-    <img src="images/donate.png" alt="Donate Crypto on Plisio" width="240" height="80" />
-    	</center>
-	</a>
- </div>
+<div class="padded container">
+	<h1 class="breach-title">حمایت مالی از سرویس</h1>
+    <p> لیکفا به ارائه خدمات رایگان و بدون تبلیغات خود ادامه می‌دهد اما توسعه و حفظ کیفیت سرویس‌ها، نیازمند کمک‌های مالی شماست. در بخش زیر آدرس ولت های مختلف برای شما ارائه شده، برای انتخاب کافیست بر روی آدرس مورد نظر کلیک کرده تا به صورت خودکار در کلیپ‌بورد شما کپی شود.</p>
+    <div class="donate-crypto-widget">
+        <div class="coin-chooser">
+            <div class="left-arrow change-coin-arrow" onclick="prevCoin()">&#9654;</div>
+            <div id="coins-container">
+                <img id="coin-logo" class="coin-logo" src="images/donate/btcButtonLogo.png" alt="Bitcoin" />
+            </div>
+            <div class="right-arrow change-coin-arrow" onclick="nextCoin()">&#9664;</div>
+        </div>
+        <div id="address-display">
+            <input type="text" id="address-text" class="address-display" value="bc1qacqns7cvsr3t7nm6tlpamss7xnk7yffqtl6ydx" readonly onclick="copyAddress()" />
+            <img id="qr" src="images/donate/btcQR.png" alt="QR Code" class="address-display" />
+            <span id="copy-message" class="copy-message">!Copied to clipboard</span>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/donate.js"></script>
+<link rel="stylesheet" href="styles/donate.css" />
+
 <?php require 'src/footer.php'; ?>
