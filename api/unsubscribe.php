46988e792b771f30296adbb9c1ab6fb52a99131e
Leakfa <83780711+Leakfarsi@users.noreply.github.com>
2025-07-27 22:27:54 +0330

    Update unsubscribe.php
    Replaced reCAPTCHA with Turnstile
    Minor improvements

--- api/unsubscribe.php
+++ api/unsubscribe.php
@@ -1,25 +1,36 @@
 <?php
 require_once '../src/common.php';
+
 $res = [];
-
 $res['status'] = 0;
-if(!is_sha1($_GET['hash'])){
-	$res['status'] = '1';
-	$res['error'] = 'مقدار هش دریافتی صحیح نمی باشد';
-}
 
-if(!filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)){
-	$res['status'] = '1';
-	$res['error'] = 'لطفا آدرس وارد شده را بررسی کرده و دوباره امتحان کنید';
-}
-
-$captcha = recaptcha_verify($_GET['token']);
-if(!$captcha->success){
+if (!isset($_GET['hash']) || !isset($_GET['email']) || !isset($_GET['token'])) {
     $res['status'] = '1';
-    $res['error'] = 'ری‌کپچا (reCAPTCHA) نتوانست هویت شما را تایید کند';
+    $res['error'] = 'پارامترهای ضروری ارسال نشده‌اند';
+    header('Content-Type: application/json');
+    echo json_encode($res);
+    exit;
 }
 
-if($res['status'] != '1'){
+if (!is_sha1($_GET['hash'])) {
+    $res['status'] = '1';
+    $res['error'] = 'مقدار هش دریافتی صحیح نمی باشد';
+}
+
+if (!filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)) {
+    $res['status'] = '1';
+    $res['error'] = 'لطفا آدرس وارد شده را بررسی کرده و دوباره امتحان کنید';
+}
+
+if ($res['status'] != '1') {
+    $captcha = turnstile_verify($_GET['token']);
+    if (!$captcha->success) {
+        $res['status'] = '1';
+        $res['error'] = 'تأیید امنیتی نتوانست هویت شما را تایید کند';
+    }
+}
+
+if ($res['status'] != '1') {
     $res = unsubscribe($_GET['email'], $_GET['hash']);
 }
 
