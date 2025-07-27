252fa206710ec65cd4d1f7ab080844efcc8616e3
Leakfa <83780711+Leakfarsi@users.noreply.github.com>
2025-07-27 22:15:57 +0330

    Update og.php

--- src/og.php
+++ src/og.php
@@ -1,14 +1,16 @@
-<meta name="description" content="این سرویس یک سامانه ردیابی و جستجوی نشت اطلاعات شخصی است که برای ایرانیان طراحی شده است. به کاربران اطلاع می دهد که آیا آنها در لیست نقض های امنیتی بزرگ وجود دارند یا خیر و تلاش می کند تا سواد امنیتی مردم را افزایش می دهند." />
-<meta property="og:title" content="<?=isset($title) ? $title.' | ':''?>سامانه ردیابی نشت اطلاعات ایرانیان" />
-<meta property="og:type" content="website" />
-<meta property="og:description" content="This service is a personal information leakage tracking and inquiry system designed for Iran, which can effectively inquire a personal information leakage channel. Let users know whether they exist in the list of major security breaches, and increase their information security literacy." />
-<meta property="og:image" content="https://leakfa.com/images/logo.png" />
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXXX-X"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-XXXXXXXXX-X');
-</script>
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXX"></script>
+		<script>
+	  	window.dataLayer = window.dataLayer || [];
+	  	function gtag(){dataLayer.push(arguments);}
+	 	gtag('js', new Date());
+
+	  	gtag('config', 'G-XXXXXXXXX');
+		</script>
+
+		<meta name="description" content="این سرویس یک سامانه ردیابی و جستجوی نشت اطلاعات شخصی ویژه کاربران ایرانی است. با بررسی نقض‌های امنیتی بزرگ، به شما اطلاع می‌دهد که آیا اطلاعاتتان در معرض افشا قرار گرفته است یا خیر. همچنین با ارائه راهکارهای امنیتی، به افزایش آگاهی و سواد سایبری کاربران کمک می‌کند." />
+		<meta property="og:title" content="<?=isset($title) ? $title.' | ':''?>سامانه ردیابی نشت اطلاعات ایرانیان" />
+		<meta property="og:type" content="website" />
+		<meta property="og:description" content="This service is a personal data leakage tracking and inquiry system designed for users in Iran. It allows individuals to check whether their personal information has been exposed in major security breaches. By providing detailed insights and security recommendations, the platform helps users enhance their cybersecurity awareness and protect their data." />
+		<meta property="og:image" content="https://leakfa.com/images/logo-white.png" />
