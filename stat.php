11fe18bf161efe35f7652796d3677354b4194748
Leakfa <83780711+Leakfarsi@users.noreply.github.com>
2026-02-23 05:26:19 +0330

    Update stat.php
    Updated stat function

--- stat.php
+++ stat.php
@@ -12,55 +12,42 @@
     </header>
 
     <div class="container">
-        <h3 class="breach-title">آمار پوشش داده ها</h3> 
-        <p>تعداد هش‌های غیرتکراری: <?=$stat['unique_hash']?>
-        <br/>جمعیت ایران: <?=$stat['total_pop']?>
-        <br/>پوشش تخمینی داده‌ها: <?=intval($stat['cover_rate'] * 10000)/100?> %</p>
-        <p>* جمعیت ایران بر اساس اطلاعات برگرفته از وب‌سایت <a href="https://www.worldometers.info/world-population/iran-population/">worldometers</a> می باشد.
-        <br/>* تعداد هش‌های غیر تکراری بر اساس شماره تلفن‌های همراه بوده و ممکن است یک فرد مالک چندین شماره باشد.</p>
+        <h3 class="breach-title">وضعیت پایگاه داده</h3> 
+        <p>نشت جزئی: <?= $stat['minor'] ?>
+        <br/>نشت عمده: <?= $stat['major'] ?> 
+     	<br/>کل منابع نشت: <?= $stat['total_sources'] ?>
+        <br/>رکوردهای ارتباط: <?= number_format($stat['relations']) ?>
+      	<br/>تعداد هش‌های یکتا: <?= number_format($stat['unique_hash']) ?> <br/></p>
 
         <h3 class="breach-title">آمار جستجو</h3> 
-        <p>کل جستجوهای مؤثر: <?=$stat['total_unique_search']?>
-        <br/>جستجوهای نشت شده: <?=$stat['hit']?>
-        <br/>جستجوهای نشت نشده: <?=$stat['no_hit']?>
-        <br/>میزان آمار نشت: <?=intval($stat['hit_rate'] * 10000)/100?> %</p>
-        <p>*کل جستجوهای مؤثر: جستجوهای غیرتکراری در گزارش جستجو
-        <br/>*میزان آمار نشت: موارد نشت شده بر اساس کل جستجو های موثر </p>
+        <p>کل جستجوهای مؤثر: <?= number_format($stat['total_unique_search']) ?>
+        <br/>موارد منتهی به نشت: <?= number_format($stat['hit']) ?>
+        <br/>موارد بدون نشت: <?= number_format($stat['no_hit']) ?>
+        <br/>نرخ کشف نشت: <?=intval($stat['hit_rate'] * 10000)/100?> %</p>
+        <p>* کل جستجوهای مؤثر: جستجوهای یکتا در گزارش جستجو 
+        <br/>* نرخ کشف نشت: درصد جستجوهایی که حداقل یک مطابقت داشتند </p>
         
-        <?php
-            // Trash Code, Need someone to fix
-            $predef_type = ['دولتی', 'تالار گفتگو', 'شبکه اجتماعی', 'شرکت خصوصی', 'موسسه تحصیلی', 'سایر'];
-        ?>
-        <h3 class="breach-title">آمار منبع داده‌ها</h3> 
-        <table class="table">  
-            <thead>
-                <tr>
-                    <td>دسته‌بندی داده‌ها</td>
-                    <td>نشت‌های عمده</td>
-                    <td>نشت‌های جزئی</td>
-                    <td>در مجموع</td>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    foreach($predef_type as $tp){
-                        echo '<tr><td>' . $tp . '</td>';
-                        foreach($stat['source'] as $source){
-                            $match = 0;
-                            foreach($source as $data){
-                                if ($data['type'] == $tp){
-                                    $match = $data['count'];
-                                }
-                            }
-                            echo '<td>' . $match . '</td>';
-                        }
-                        echo '</tr>';
-                    }
-                ?>
-            </tbody>
-        </table>
+        <h3 class="breach-title">آمار منبع داده</h3> 
+      <table class="table">
+          <thead>
+              <tr>
+                  <td>دسته‌بندی</td>
+                  <td>نشت‌های عمده</td>
+                  <td>نشت‌های جزئی</td>
+                  <td>در مجموع</td>
+              </tr>
+          </thead>
+          <tbody>
+              <?php foreach($stat['categories'] as $row): ?>
+                  <tr>
+                      <td><?= htmlspecialchars($row['category']) ?></td>
+                      <td><?= $row['major_count'] ?></td>
+                      <td><?= $row['minor_count'] ?></td>
+                      <td><?= $row['total_count'] ?></td>
+                  </tr>
+              <?php endforeach; ?>
+          </tbody>
+      </table>
     </div>
-
-
 
     <?php require 'src/footer.php'; ?>
