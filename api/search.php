0cce1806c6f27804eda8eb3bd961bb2c723c9fec
Leakfa <83780711+Leakfarsi@users.noreply.github.com>
2026-04-23 02:10:08 +0330

    Update search.php

--- api/search.php
+++ api/search.php
@@ -1,31 +1,34 @@
 <?php
 require_once '../src/common.php';
 $res = [];
-
 $res['status'] = 0;
-if(!is_sha1($_GET['hash'])){
+
+$hash = isset($_GET['hash']) ? $_GET['hash'] : '';
+$mode = isset($_GET['mode']) ? $_GET['mode'] : '';
+$token = isset($_GET['token']) ? $_GET['token'] : '';
+$nonce = isset($_GET['nonce']) ? $_GET['nonce'] : '';
+
+if(!is_sha1($hash)){
 	$res['status'] = '1';
 	$res['error'] = 'Input format error';
-}
-
-if($_GET['mode'] == 'turnstile'){
-    $captcha = turnstile_verify($_GET['token']);
+} elseif($mode == 'turnstile'){
+    $captcha = turnstile_verify($token);
     if(!$captcha->success){
         $res['status'] = '1';
         $res['error'] = 'Turnstile verification failed';
     }
-}else if($_GET['mode'] == 'pow'){
-    if (substr(sha1($_GET['hash'] . $_GET['nonce']), 0, POW_DIFF) != str_repeat('a', POW_DIFF)){
+} elseif($mode == 'pow'){
+    if (substr(sha1($hash . $nonce), 0, POW_DIFF) != str_repeat('a', POW_DIFF)){
         $res['status'] = '1';
         $res['error'] = 'Nonce error: Nonce must meet the sha1 (request hash + nonce)' . POW_DIFF . ' Bit equal ' . str_repeat('a', POW_DIFF); 
     }
-}else{
+} else {
     $res['status'] = '1';
     $res['error'] = 'Verification mode error';
 }
 
 if($res['status'] != '1'){
-    $res = search($_GET['hash']);
+    $res = search($hash);
 }
 
 header('Content-Type: application/json');
