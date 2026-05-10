42d3c37381508be87c297b8365a177a764fb4af7
Leakfa <83780711+Leakfarsi@users.noreply.github.com>
2026-05-10 16:02:28 +0330

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
                     id="phone"  
                     maxlength="255" 
                     style="text-transform:lowercase"
-                    placeholder="e.g. 09123456789"  
+                    placeholder="e.g. 09123456789 or john@example.com"  
                     autocomplete="off" 
                     autocapitalize="off"
                     autocorrect="off"
